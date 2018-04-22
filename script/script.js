45fb388ed03340bba19f6cd990398c72b0060696
katzdima <katzdima@gmail.com>
2018-04-22 15:24:03 +0300

    with connection to api server and sql

--- script/script.js
+++ script/script.js
@@ -1,51 +1,40 @@
+// the AJAX URL http://localhost:8080/playlist/api/playlist.php/?type=playlist for all lists
+//the url for all songs from first playlist http://localhost:8080/playlist/api/playlist.php/?type=songs&id=1
+// the url of the first playlist is http://localhost:8080/playlist/api/playlist.php/?type=playlist&id=1
+$(document).ready(()=>{
+    $(".playlists").html("");
+    $("#mediaPlayer").hide();
+    var listURL="http://localhost:8080/playlist/api/playlist.php/?type=playlist";
 
-$(document).ready(function(){
-    // $(".playlists").html("");
-    var listURL="https://reqres.in/api/users";
+    $(".playlists").on("click",".playIcon",()=>{
+        console.log("play");
+        $("#mediaPlayer").slideDown("slow");
+        let id=$(this).parent().attr("id");
+        console.log(id);
+    });
 
-    $.ajax({url: listURL,type: "GET", success: function(results){
+    $("#mediaPlayer").on("click",".closeMediaPlayer",()=>{
+        $("#mediaPlayer").slideUp("slow");
+    });
+
+    $.ajax({url: listURL,type: "GET", success:(results)=>{
         console.log(results);
-        for(let page=0; page< results["total_pages"];page++){
-            $.ajax({url: "https://reqres.in/api/users?page="+page,type: "GET", success: function(result){
-                console.log(results["total_pages"]);
-                for (let listItem in result["data"] ){
-                    $(".playlists").append(`
-                        <div class="note col-xm-12 col-sm-6 col-md-4 col-lg-3 ">
-                            <h3 class="text-center">${result["data"][listItem]["first_name"]} ${result["data"][listItem]["last_name"]}</h3>
-                            <div class="text-center albomReviewPic">
-                                <div class="iconsPreviewPic">
-                                    <i class="far fa-times-circle deleteIconPreviewPic"></i>
-                                    <i class="fas fa-info-circle editIconPreviewPic"></i>
-                                    <a href="#" class="playIcon">
-                                        <i class="far fa-play-circle fa-2x"></i>
-                                    </a>
-                                </div>
-                                <img src="${result["data"][listItem]["avatar"]}" alt="Smiley face" class="previewImg">
-                            </div>
+        for (let listItem in results.data){
+            $(".playlists").append(`
+                <div class="note col-xm-12 col-sm-6 col-md-4 col-lg-3 ">
+                    <h3 class="text-center">${results.data[listItem].name}</h3>
+                    <div class="text-center albomReviewPic">
+                        <div class="iconsPreviewPic" id="${results.data[listItem].id}">
+                            <i class="far fa-times-circle deleteIconPreviewPic"></i>
+                            <i class="fas fa-info-circle editIconPreviewPic"></i>
+                            <a class="playIcon">
+                                <i class="far fa-play-circle fa-2x"></i>
+                            </a>
                         </div>
-                    `);
-                }
-            }});
+                        <img src="${results.data[listItem].image}" alt="Smiley face" class="previewImg">
+                    </div>
+                </div>
+            `);
         }
     }});
-
-    // var playListArr=[1,2,3,4,5,6,7,8];
-    // var previewImg = 'images/smile.jpg'; 
-    // for (let listItem in playListArr ){
-    //     $(".playlists").append(`
-    //         <div class="note col-xm-12 col-sm-6 col-md-4 col-lg-3 ">
-    //             <h3 class="text-center">${listItem}</h3>
-    //             <div class="text-center albomReviewPic">
-    //                 <div class="iconsPreviewPic">
-    //                     <i class="far fa-times-circle deleteIconPreviewPic"></i>
-    //                     <i class="fas fa-info-circle editIconPreviewPic"></i>
-    //                     <a href="#" class="playIcon">
-    //                         <i class="far fa-play-circle fa-2x"></i>
-    //                     </a>
-    //                 </div>
-    //                 <img src="${previewImg}" alt="Smiley face" class="previewImg">
-    //             </div>
-    //         </div>
-    //     `);
-    // }
 });

c471753a108abcb019c2fc37ffd174c03fbe76b9
katzdima <katzdima@gmail.com>
2018-04-27 10:22:56 +0300

    player links

--- script/mediaplayer.js
+++ script/mediaplayer.js
@@ -1,27 +1,41 @@
+ //http://www.disi.co.il/songs/Yam/EdenBenZaken/039.mp3
+ //http://www.disi.co.il/songs/Yam/ItayHarari/020.mp3
+ 
  //play icon on the play list
-$(".playlists").on("click",".playIcon",()=>{
+$(".playlists").on("click",".playIcon",function(){
+    $("#songsList").html("");
+    $("#pauseIconAtPlayer").hide();
     $("#generalPlayerArea").slideDown("slow");
 
-    var id=0;
-    id=$(this).parent().attr("id");
+    let id=$(this).parent().attr("id").charAt(4);
     console.log(id);
 
     //cover image
-    id=1;
     listURL=`http://localhost:8080/playlist/api/playlist.php/?type=playlist&id=${id}`;
     $.ajax({url: listURL,type: "GET", success:(results)=>{
-        console.log(results);
+        // console.log(results);
         $("#playerCoverImage").attr("src",results.data.image);
 
     }});
 
     //songs list
-    id=1;
     listURL=`http://localhost:8080/playlist/api/playlist.php/?type=songs&id=${id}`;
     $.ajax({url: listURL,type: "GET", success:(results)=>{
-        console.log(results);
-        
+        //console.log(results.data.songs.data);
+
+        //creating songs list
+        for (let listItem in results.data.songs.data){
+            $("#songsList").append(` 
+                <li id="li${results.data.songs.data[listItem].id}">
+                    ${results.data.songs.data[listItem].id}.${results.data.songs.data[listItem].name}
+                </li>
+            `);
+        }
+        //initilizing player with first song
+         $("#player audio").attr("src","http://www.disi.co.il/songs/Yam/ItayHarari/020.mp3");
+
     }});
+
 
 });
 
--- script/script.js
+++ script/script.js
@@ -2,28 +2,21 @@
 //the url for all songs from first playlist http://localhost:8080/playlist/api/playlist.php/?type=songs&id=1
 // the url of the first playlist is http://localhost:8080/playlist/api/playlist.php/?type=playlist&id=1
 $(document).ready(()=>{
-    $(".playlists").html("");
+    //$(".playlists").html("");
     $("#generalPlayerArea").hide();
     var listURL="http://localhost:8080/playlist/api/playlist.php/?type=playlist";
 
-   
-    // $(".playlists").on("click",".edit",()=>{
-    //     var i=0;
-    //     i=$(this).attr("class");
-    //     console.log(i);
-       
-    // });
 
+//need to set the maximum number of chars in name to prevent over slide
     //main list of the playlists
-    $.ajax({url: listURL,type: "GET", success:(results)=>{
+    $.ajax({url: listURL,type: "GET",dataType: 'json', success:(results)=>{
         console.log(results);
         for (let listItem in results.data){
-            console.log(results.data[listItem].id);
             $(".playlists").append(`
-                <div class="note col-xm-12 col-sm-6 col-md-4 col-lg-3 " id="${results.data[listItem].id}">
+                <div class="note col-xm-12 col-sm-6 col-md-4 col-lg-3">
                     <h4 class="text-center curved">${results.data[listItem].name}</h4>
                     <div class="text-center albomReviewPic">
-                        <div class="iconsPreviewPic" >
+                        <div class="iconsPreviewPic" id="card${results.data[listItem].id}">
                             <i class="far fa-times-circle deleteIconPreviewPic"></i>
                             <i class="fas fa-info-circle editIconPreviewPic"></i>
                             <a class="playIcon">
